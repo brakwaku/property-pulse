5648ba9b92eb32d5ba206c57f308575fba0a8644
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2024-05-07 23:10:33 -0600

    Add Google sign in using next auth. Protect routes. Add image hostname

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,26 @@
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
 import '@/assets/styles/globals.css';
 
 export const metadata = {
-    title: 'PropertyPulse | Fnd your dream rental property',
-    description: 'Find your dream rental property with PropertyPulse',
-    keywords: 'property, rental, rent, house, apartment, condo, townhouse, find rentals',
+  title: 'PropertyPulse | Fnd your dream rental property',
+  description: 'Find your dream rental property with PropertyPulse',
+  keywords:
+    'property, rental, rent, house, apartment, condo, townhouse, find rentals',
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 export default MainLayout;
